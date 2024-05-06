4c5aa8ea28d6c75ad0946275105e33f71c804be0
Jaya Krishna <jayakrishna@Jayas-MacBook-Air.local>
2024-05-06 16:37:26 +0530

    changed model aqi_calculation

--- models/aqi_calculation.sql
+++ models/aqi_calculation.sql
@@ -1,16 +1,40 @@
 -- models/aqi_calculation.sql
 
 WITH normalized_data AS (
-    SELECT *
-    FROM {{ ref('normalized_air_quality') }}
+    SELECT 
+        country,
+        hour,
+        pollutant,
+        AVG(normalized_value) AS normalized_avg  -- Aggregate average normalized values by pollutant
+    FROM 
+        {{ ref('normalized_air_quality') }}
+    GROUP BY 
+        country, hour, pollutant
+),
+
+pivot_data AS (
+    SELECT
+        country,
+        hour,
+        MAX(CASE WHEN pollutant = 'pm25' THEN normalized_avg END) AS normalized_pm25,
+        MAX(CASE WHEN pollutant = 'so2' THEN normalized_avg END) AS normalized_so2,
+        MAX(CASE WHEN pollutant = 'co' THEN normalized_avg END) AS normalized_co
+    FROM 
+        normalized_data
+    GROUP BY 
+        country, hour
 ),
 
 aqi_scores AS (
     SELECT
         country,
         hour,
-        (normalized_pm25 + normalized_so2 + normalized_co) / 3 AS aqi
-    FROM normalized_data
+        (COALESCE(normalized_pm25, 0) + COALESCE(normalized_so2, 0) + COALESCE(normalized_co, 0)) / 
+        (CASE WHEN normalized_pm25 IS NOT NULL THEN 1 ELSE 0 END +
+         CASE WHEN normalized_so2 IS NOT NULL THEN 1 ELSE 0 END +
+         CASE WHEN normalized_co IS NOT NULL THEN 1 ELSE 0 END) AS aqi
+    FROM 
+        pivot_data
 )
 
 SELECT
@@ -22,4 +46,5 @@ SELECT
         WHEN aqi > 33 THEN 'Moderate'
         ELSE 'Low'
     END AS aqi_level
-FROM aqi_scores
+FROM 
+    aqi_scores
